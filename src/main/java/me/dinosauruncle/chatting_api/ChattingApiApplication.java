c3370bef3e6e64d6dce8b21ee7287fd3b424439e
박 종훈 <m05214@naver.com>
2020-03-11 17:27:20 +0900

    최초 커밋

---
+++ src/main/java/me/dinosauruncle/chatting_api/ChattingApiApplication.java
@@ -0,0 +1,13 @@
+package me.dinosauruncle.chatting_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dinosauruncle/chatting_api/ChattingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dinosauruncle.chatting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
